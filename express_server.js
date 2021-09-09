91c10c6d0b9aafa032019f8a06f63952fc1408f4
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2021-09-09 01:15:48 -0400

    Create a URL Submission Form, Add a GET Route to Show the Form, Add a POST Route to Receive the Form Submission, Generate a Random ShortURL (6 digits - alphanemeric)

--- express_server.js
+++ express_server.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  let random = Math.floor((Math.random() * 999999) + 1);
+  return random;
+}
+console.log(generateRandomString());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,6 +31,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
@@ -38,4 +51,9 @@ app.get("/set", (req, res) => {
  
 app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
