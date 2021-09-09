e9f0c0f74c37df3197f89947537679d21b9caa4a
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2021-09-09 03:52:03 -0400

    Redirect After Form Submission, Redirect Short URLs, Validate

--- express_server.js
+++ express_server.js
@@ -39,7 +39,13 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  console.log(templateVars);
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
