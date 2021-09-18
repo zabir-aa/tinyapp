18a105a4ddf0a1808e19244bc3ecd5e0128a9f93
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2021-09-18 00:48:23 -0400

    Change urlDatabase structure, include userID key

--- express_server.js
+++ express_server.js
@@ -29,8 +29,8 @@ const users = {
 }
 
 let urlDatabase = {
-  "9sm5xK": "http://www.google.com",
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": {longURL: "http://www.google.com", userID: "aj48Lw"},
+  "b2xVn2": {longURL: "http://www.lighthouselabs.ca", userID: "aj48Lw"}
 };
 
 /*
