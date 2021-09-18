0f3f3235560831d54c160f6b41b1d4cfa2771fcd
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2021-09-18 02:34:47 -0400

    fix app.get(/urls/:shortURL.. and app.get(/u/:shortURL with  correct longURL key

--- express_server.js
+++ express_server.js
@@ -77,14 +77,14 @@ app.get("/login", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL],
+    longURL: urlDatabase[req.params.shortURL]["longURL"],
     user: users[req.cookies["user_id"]],
   };
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL]["longURL"];
   res.redirect(longURL);
 });
 
