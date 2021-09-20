ec330d137df8a991a3952850bfdb6e3f9db73d5c
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2021-09-20 03:54:55 -0400

    Add separate database file, move data objects there, make minorchanges in url_index template, lint and comment in express_server

---
+++ database.js
@@ -0,0 +1,23 @@
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
+let urlDatabase = {
+  "9sm5xK": {longURL: "http://www.google.com", userID: "aj48Lw"}, 
+  "b2xVn2": {longURL: "http://www.lighthouselabs.ca", userID: "aj48Lw"}
+};
+
+
+module.exports ={
+  users,
+  urlDatabase
+}
--- express_server.js
+++ express_server.js
@@ -1,67 +1,50 @@
+//EXPRESS
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
 const PORT = 8080; // default port 8080
 
-var cookieSession = require('cookie-session')
+//COOKIE-SESSION MIDDLEWARE
+let cookieSession = require('cookie-session');
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2']
-}))
-
+}));
+// BODYPARSER
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-
+// BCRYPT
 const bcrypt = require('bcrypt');
+
+// Server initialization
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 // HELPER FUNCTIONS
 const {getUserByEmail} = require('./helpers');
 const {generateRandomString} = require('./helpers');
 const {urlsForUser} = require('./helpers');
 
-// Server initialization
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
-// GLOBAL OBJECTS
-
-const users = { 
-  "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
-  },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
-    password: "dishwasher-funk"
-  }
-}
-
-let urlDatabase = {
-  "9sm5xK": {longURL: "http://www.google.com", userID: "aj48Lw"}, 
-  "b2xVn2": {longURL: "http://www.lighthouselabs.ca", userID: "aj48Lw"}
-};
-
-
+// DATABASE
+const {users} = require('./database');
+const {urlDatabase} = require('./database');
 
 // GET ROUTES
 
 app.get("/urls", (req, res) => { // the homepage
-  const templateVars = { 
+  const templateVars = {
     user: users[req.session["user_id"]],
     urls: urlsForUser(req.session["user_id"], urlDatabase) };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => { // gets the new URL addition page (//urls/new)
-  if (req.session["user_id"]) { // checks session to see if user is logged in 
-    const templateVars = {  // variable to pass 
-    user: users[req.session["user_id"]], // includes the same user_id from session
-    urls: urlDatabase }; // and the URL database
-  res.render("urls_new", templateVars); //variable being passed to /urls/new template
+  if (req.session["user_id"]) { // checks session to see if user is logged in
+    const templateVars = {  // variable to pass
+      user: users[req.session["user_id"]], // includes the same user_id from session
+      urls: urlDatabase }; // and the URL database
+    res.render("urls_new", templateVars); //variable being passed to /urls/new template
   } else {
     res.redirect("/login"); // redirect to login page if user is not logged it
   }
@@ -108,7 +91,7 @@ app.post("/u/:shortURL", (req, res) => {
 
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
-  urlDatabase[shortURL] = {longURL: req.body.longURL, userID: req.session["user_id"]}
+  urlDatabase[shortURL] = {longURL: req.body.longURL, userID: req.session["user_id"]};
   console.log(urlDatabase);
   res.redirect(`/urls`);
 });
@@ -119,50 +102,36 @@ app.post("/register", (req, res) => {
   if (getUserByEmail(req.body.email, users) || req.body.email === "" || req.body.password === "") {
     entryValidity = false;
   }
-  console.log("VALIDITY: ", entryValidity)
+  console.log("VALIDITY: ", entryValidity);
   if (entryValidity === false) {
     res.status(400).send();
   } else {
     let userID = generateRandomString();
-    hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
     users[userID] = {
-    id: userID, 
-    email: req.body.email, 
-    password: hashedPassword
-    }
-    console.log("PRINTING req.session:\n", req.session);
+      id: userID,
+      email: req.body.email,
+      password: hashedPassword
+    };
     req.session["user_id"] = userID;
-    console.log("PRINTING REQ.SESSION:\n",req.session["user_id"])
     res.redirect(`/urls`);
   }
 });
 
-/*
-app.post("/login", (req, res) => {
-  for (let user in users) {
-    if (req.body.email === users[user]["email"] && bcrypt.compareSync(req.body.password, users[user]["password"])) {
-      req.session.user_id = users[user]["id"];
-      return res.redirect("/urls");
-    }
-  }
-  return res.status(403).send();
-});
-*/
-
 
 app.post("/login", (req, res) => {
-  user = getUserByEmail(req.body.email, users);
+  const user = getUserByEmail(req.body.email, users);
   if (user) {
     if (bcrypt.compareSync(req.body.password, users[user]["password"])) {
-      req.session.user_id = users[user]["id"];
+      req.session["user_id"] = users[user]["id"];
       return res.redirect("/urls");
-  }
+    }
   }
   return res.status(403).send();
 });
 
 app.post("/urls/:shortURL/edit", (req, res) => {
-  if( Object.keys(urlsForUser(req.session.user_id, urlDatabase)).includes(req.params.shortURL)) {
+  if (Object.keys(urlsForUser(req.session.user_id, urlDatabase)).includes(req.params.shortURL)) {
     let toEdit = req.params.shortURL;
     urlDatabase[toEdit]["longURL"] = req.body.longURL;
     res.redirect(`/urls/`);
@@ -172,7 +141,7 @@ app.post("/urls/:shortURL/edit", (req, res) => {
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  if( Object.keys(urlsForUser(req.session.user_id, urlDatabase)).includes(req.params.shortURL)) {
+  if (Object.keys(urlsForUser(req.session.user_id, urlDatabase)).includes(req.params.shortURL)) {
     let toDelete = req.params.shortURL;
     delete urlDatabase[toDelete];
     res.redirect("/urls");
