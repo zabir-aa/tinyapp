3ef4c9361a6969f4b7b3048958b8efbf82aa81b8
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2021-09-25 13:27:16 -0400

    Refactor all user permission filters to remove unneccessary use of Object.keys; use a single error template to receive personalized title and message from code, refactor code to use the new error tempate in all error cases with personalized messsages, remove rest of the error templates

--- express_server.js
+++ express_server.js
@@ -60,7 +60,7 @@ app.get("/login", (req, res) => { // gets the user login page (/login)
 });
 
 app.get("/urls/:shortURL", (req, res) => { // gets the shortURL view / edit page (/urls/:shortURL)
-  if (Object.keys(urlsForUser(req.session.user_id, urlDatabase)).includes(req.params.shortURL)) { // checks if shortURL exists for the user
+  if (urlsForUser(req.session.user_id, urlDatabase)[req.params.shortURL]) { // checks if shortURL exists for the user
     const templateVars = { // variable to pass to template
       shortURL: req.params.shortURL, // includes the shortURL from user request
       longURL: urlDatabase[req.params.shortURL]["longURL"], // also the associated longURL from urlDatabase
@@ -68,42 +68,61 @@ app.get("/urls/:shortURL", (req, res) => { // gets the shortURL view / edit page
     };
     res.render("urls_show", templateVars); // variable being passed to /urls/:shortURL template
   } else {
-    res.render("error_404"); // Send error : 404 in case of nonexistent entry
+    const errorMessage = {
+      title: "Error 404: Not Found",
+      message: "The requested short URL does not exist for this user!"
+    }
+    return res.render("error_display", errorMessage); // Send error : 404 in case of nonexistent entry
   }
   
 });
 
 app.get("/u/:shortURL", (req, res) => { // the redirect to longURL route (/u/:shortURL)
-  if (Object.keys(urlDatabase).includes(req.params.shortURL)) { // // Checks if the shortURL exists in the urlDatabase
+  if (urlsForUser(req.session.user_id, urlDatabase)[req.params.shortURL]) { // // Checks if the shortURL exists in the urlDatabase
     const longURL = urlDatabase[req.params.shortURL]["longURL"]; // find associated longURL from the urlDatabase with the shortURL from user request
     res.redirect(longURL); // redirect to longURL (primary action of the service)
   } else {
-    res.render("error_404"); //// Sends error:404 for nonexistent shortURL
+    const errorMessage = {
+      title: "Error 404: Not Found",
+      message: "The requested short URL does not exist for this user!"
+    }
+    return res.render("error_display", errorMessage); // Sends error:404 for nonexistent shortURL
   }
 });
 
-
+/*----------------------------*/
 // POST ROUTES
 
 app.post("/register", (req, res) => { // new user registration request
-  let entryValidity = true; // entry validity flag
-  if (getUserByEmail(req.body.email, users) || req.body.email === "" || req.body.password === "") { // checks invalid entry
-    entryValidity = false; // entry validity flag activation
+  if (req.body.email === "") {
+    const errorMessage = {
+      title: "Error 400: Bad Request",
+      message: "You left the email field blank!"
+    }
+    return res.render("error_display", errorMessage);
+  } else if (req.body.password === "") {
+    const errorMessage = {
+      title: "Error 400: Bad Request",
+      message: "You left the password field blank!"
+    }
+    return res.render("error_display", errorMessage);
+  } else if (getUserByEmail(req.body.email, users)) {
+    const errorMessage = {
+      title: "Error 400: Bad Request",
+      message: "This email is already registered! Please try to log in."
+    }
+    return res.render("error_display", errorMessage);
   }
-  if (entryValidity === false) {
-    res.render("error_400");
-    //res.status(400).send(); // Send error : 400 in case of invalid entry
-  } else {
-    const userID = generateRandomString(); // random user id generator
-    const hashedPassword = bcrypt.hashSync(req.body.password, 10); // store hashed password
-    users[userID] = { // store new user information to database
-      id: userID,
-      email: req.body.email,
-      password: hashedPassword
-    };
-    req.session["user_id"] = userID; // set cookie for new user
-    res.redirect(`/urls`); // rdirect to /urls (homepage)
-  }
+  //res.status(400).send(); // Send error : 400 in case of invalid entry
+  const userID = generateRandomString(); // random user id generator
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10); // store hashed password
+  users[userID] = { // store new user information to database
+    id: userID,
+    email: req.body.email,
+    password: hashedPassword
+  };
+  req.session["user_id"] = userID; // set cookie for new user
+  res.redirect(`/urls`); // rdirect to /urls (homepage)  
 });
 
 app.post("/login", (req, res) => { // login request
@@ -114,7 +133,11 @@ app.post("/login", (req, res) => { // login request
       return res.redirect("/urls"); // redirects to /urls (homepage)
     }
   }
-  return res.render("error_403"); // if email or password mismatched, sends error 403 : forbidden
+  const errorMessage = {
+    title: "Error 403: Forbidden",
+    message: "The email / password combination does not match with our records! Please try again."
+  }
+  return res.render("error_display", errorMessage); // if email or password is mismatched, redirects to error template with error 403 : forbidden
 });
 
 app.post("/urls", (req, res) => { // create new shortURL request
@@ -123,33 +146,45 @@ app.post("/urls", (req, res) => { // create new shortURL request
   res.redirect(`/urls`); // redirects to homepage
 });
 
-app.post("/u/:shortURL", (req, res) => { // the longURL access via shortURL request (primary service)
-  if (Object.keys(urlDatabase).includes(req.params.shortURL)) { // Checks if the shortURL exists in the urlDatabase
+app.post("/u/:shortURL", (req, res) => { // the shortURL show / edit page request
+  if (urlDatabase[req.params.shortURL]) { // Checks if the shortURL exists in the urlDatabase
     const shortURL = req.params.shortURL; // fetches the shortURL from user request
-    res.redirect(`/urls/${shortURL}`); // redirects user to longURL
+    res.redirect(`/urls/${shortURL}`); // redirects user to shortURL view/edit route
   } else {
-    res.render("error_404"); // Sends error:404 for nonexistent shortURL
+    const errorMessage = {
+      title: "Error 404: Not Found",
+      message: "The requested short URL does not exist for this user!"
+    }
+    return res.render("error_display", errorMessage); // Sends error:404 for nonexistent shortURL for user
   }
   
 });
 
 app.post("/urls/:shortURL/edit", (req, res) => { // shortURL edit request
-  if (Object.keys(urlsForUser(req.session.user_id, urlDatabase)).includes(req.params.shortURL)) { // checks if the owner user is making the request
+  if (urlsForUser(req.session.user_id, urlDatabase)[req.params.shortURL]) { // checks if the owner user is making the request
     const toEdit = req.params.shortURL; // fetches the shortURL to be edited from request
     urlDatabase[toEdit]["longURL"] = req.body.longURL; // replaces the associated longURL in database with user entry
     res.redirect(`/urls/`); // redirects to homepage
   } else {
-    res.render("error_403"); // in case of unauthorized user, sends 403: forbidden error
+    const errorMessage = {
+      title: "Error 403: Forbidden",
+      message: "You are not authorized to make this request!"
+    }
+    return res.render("error_display", errorMessage); // in case of unauthorized user, sends 403: forbidden error
   }
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => { // shortURL record delete request
-  if (Object.keys(urlsForUser(req.session.user_id, urlDatabase)).includes(req.params.shortURL)) { // checks if the owner user is making the request
+  if (urlsForUser(req.session.user_id, urlDatabase)[req.params.shortURL]) { // checks if the owner user is making the request
     const toDelete = req.params.shortURL; // fetches the shortURL to be deleted from request
     delete urlDatabase[toDelete]; // deletes the record from urlDatabase
     res.redirect("/urls"); // redirects to homepage
   } else {
-    res.render("error_403"); // in case of unauthorized user, sends 403: forbidden error
+    const errorMessage = {
+      title: "Error 403: Forbidden",
+      message: "You are not authorized to make this request!"
+    }
+    return res.render("error_display", errorMessage); // in case of unauthorized user, sends 403: forbidden error
   }
 });
 
@@ -158,6 +193,7 @@ app.post("/logout", (req, res) => { // session logout request
   res.redirect("/urls"); // redirects to homepage
 });
 
+/*----------------------------*/
 // Server initialization
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
